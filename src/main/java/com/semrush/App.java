4f21f8ac4fe535a605e2c7050f27f5b1814b610e
Vladislav Fedotov <v.fedotov@semrush.com>
2017-06-02 17:10:25 +0300

    Second commit

--- src/main/java/com/semrush/App.java
+++ src/main/java/com/semrush/App.java
@@ -1,8 +1,18 @@
 package com.semrush;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Hello world!
@@ -10,7 +20,7 @@ import java.io.File;
  */
 public class App 
 {
-    public static void main( String[] args ) throws JAXBException {
+    public static void main( String[] args ) throws JAXBException, IOException, SAXException {
         System.out.println( "Hello World!" );
 
 
@@ -20,5 +30,16 @@ public class App
         Customer c = (Customer) jaxbContext.createUnmarshaller().unmarshal(f);
 
         System.out.println(c);
+
+
+        byte[] xmlString = Files.readAllBytes(Paths.get(f.getPath()));
+        InputStream is = new ByteArrayInputStream(xmlString);
+        Document doc = PositionalXMLReader.readXML(is);
+        is.close();
+
+
+        Node node = doc.getElementsByTagName("name").item(0);
+        System.out.println(node.getTextContent());
+        System.out.println("Line number: " + node.getUserData("lineNumber"));
     }
 }
